d309e83750642d196380a6d8e59d98755769e311
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-04-16 18:57:41 -0400

    Push to repository

---
+++ purchases/src/http/auth/authorization.guard.ts
@@ -0,0 +1,11 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'node:util';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return true;
+  }
+}
