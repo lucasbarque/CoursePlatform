92fc085949c3985d8c9290e2000db78bd7e708d0
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-04-20 21:28:56 -0400

    Create Products

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,23 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ProductsService } from '../../../services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { Product } from '../models/product';
+
+@Resolver('test')
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  // @UseGuards(AuthorizationGuard)
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
