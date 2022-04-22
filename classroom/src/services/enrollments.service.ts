d0c25f0dafc9ae5474a9d52901d80690f65fab53
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-04-22 18:29:42 -0400

    Asynchronous Comunication Between Services

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -5,6 +5,10 @@ interface GetByCourseAndStudentIdParams {
   courseId: string;
   studentId: string;
 }
+interface CreateEnrollmentParams {
+  courseId: string;
+  studentId: string;
+}
 
 @Injectable()
 export class EnrollmentsService {
@@ -42,6 +46,15 @@ export class EnrollmentsService {
       },
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  createEnrollment({ courseId, studentId }: CreateEnrollmentParams) {
+    return this.prisma.enrollment.create({
+      data: {
+        courseId,
+        studentId,
       },
     });
   }
